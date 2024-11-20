5beb0e7ebb189acdd0fad6a2bb2f4911c5ad290a
killianpineda <killianpineda12@gmail.com>
2024-11-20 12:05:48 -0700

    gloss stuff

---
+++ haskell-project/app/Main.hs
@@ -0,0 +1,10 @@
+
+import Graphics.Gloss
+
+main :: IO ()
+main = display (InWindow "Gloss Test" (400, 400) (10, 10)) white (Circle 80)
+
+module Main where
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
